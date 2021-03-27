a98c49d3ec30df3c4ec33db2d3d5d098b5ee24c4
phellipe5k <luizinhophe@hotmail.com>
2021-03-27 19:38:21 -0300

    insertdotenv

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-let express = require('express')
-let request = require('request')
-let querystring = require('querystring')
+const express = require('express');
+require('dotenv').config()
+const request = require('request');
+const querystring = require('querystring');
 
 let app = express()
 
-let redirect_uri = 
-  process.env.REDIRECT_URI || 
-  'http://localhost:8888/callback'
+let redirect_uri = process.env.REDIRECT_URI;
 
 app.get('/login', function(req, res) {
   res.redirect('https://accounts.spotify.com/authorize?' +
@@ -35,12 +34,10 @@ app.get('/callback', function(req, res) {
     json: true
   }
   request.post(authOptions, function(error, response, body) {
-    var access_token = body.access_token
-    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
-    res.redirect(uri + '#access_token=' + access_token)
+    const access_token = body.access_token;
+    res.redirect(process.env.FRONTEND_URI + '#access_token=' + access_token)
   })
 })
 
-let port = process.env.PORT || 8888
-console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
-app.listen(port)
+console.log(`Listening on port ${process.env.PORT}. Go /login to initiate authentication flow.`)
+app.listen(process.env.PORT)
